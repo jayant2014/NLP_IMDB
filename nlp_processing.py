623888d33de7dcf561cc1c05115e00a28b14b658
Jayant Parida <jayant2014@users.noreply.github.com>
2020-09-28 16:46:50 +0530

    Added Tfidf vectoriser

--- nlp_processing.py
+++ nlp_processing.py
@@ -1,4 +1,5 @@
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.tokenize import word_tokenize
 
 # Create a corpus of sentences
@@ -16,9 +17,19 @@ corpus = [
 # Initialize CountVectorizer with word_tokenize from nltk as tokenizer
 cnt_vec = CountVectorizer(tokenizer = word_tokenize, token_pattern = None)
 
+# Initialize TfidfVectorizer with word_tokenize from nltk as tokenize
+tfidf_vec = TfidfVectorizer(tokenizer = word_tokenize, token_pattern = None)
+
 # Fit the vectorizer on corpus
 cnt_vec.fit(corpus)
+tfidf_vec.fit(corpus)
 
 corpus_transformed = cnt_vec.transform(corpus)
+tfidf_corpus_transformed = tfidf_vec.transform(corpus)
+print("\n\n========Count Vectorizer========")
 print(corpus_transformed)
 print(cnt_vec.vocabulary_)
+
+print("\n\n========Tfidf Vectorizer========")
+print(tfidf_corpus_transformed)
+print(tfidf_vec.vocabulary_)
